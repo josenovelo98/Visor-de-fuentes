9ee81e9b876557ee7a19d0824f75cf4920c78ad4
josenovelo98 <19470202@itcampeche.edu.mx>
2020-10-26 12:07:30 -0600

    manejador de eventos

--- VisorDeFuentes/MainWindow.xaml.cs
+++ VisorDeFuentes/MainWindow.xaml.cs
@@ -24,5 +24,42 @@ namespace VisorDeFuentes
         {
             InitializeComponent();
         }
+
+        private void fuentecombobox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (fuentecombobox.SelectedItem != null) 
+            { string fuente = (string)(fuentecombobox.SelectedItem as ComboBoxItem).Content; 
+                leyendatextblock.FontFamily = new FontFamily(familyName: fuente); }
+        }
+
+        private void cursivacheckbox_Checked(object sender, RoutedEventArgs e)
+        {
+            leyendatextblock.FontStyle = FontStyles.Italic;
+        }
+    
+
+        private void cursivacheckbox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            leyendatextblock.FontStyle = FontStyles.Normal;
+
+        }
+
+        private void negritascheckbox_Checked(object sender, RoutedEventArgs e)
+        {
+            leyendatextblock.FontWeight = FontWeights.Bold;
+
+        }
+
+        private void negritascheckbox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            leyendatextblock.FontWeight = FontWeights.Normal;
+        }
+
+        private void tamañoradiobuttom_Checked(object sender, RoutedEventArgs e)
+        {
+            if (smallradiobutton.IsChecked == true) { leyendatextblock.FontSize = 12; } 
+            else if (mediumradiobutton.IsChecked == true) { leyendatextblock.FontSize = 18; } 
+            else { leyendatextblock.FontSize = 24; }
+        }
     }
 }
